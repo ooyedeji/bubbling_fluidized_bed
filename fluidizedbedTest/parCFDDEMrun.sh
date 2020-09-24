e7c4716557f536375b13540ecb7b3cb83b6a96a4
ooyedeji <demolaoye@gmail.com>
2020-09-24 10:25:04 -0400

    Updated parCFDDEMrun.sh

--- fluidizedbedTest/parCFDDEMrun.sh
+++ fluidizedbedTest/parCFDDEMrun.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
+
 #===================================================================#
 # allrun script for testcase as part of test routine
 # run settlingTest CFD part
 # Christoph Goniva - Feb. 2011
 #===================================================================#
+
 #- source CFDEM env vars
 source ~/.bashrc
+
 #- include functions
 source $CFDEM_SRC_DIR/lagrangian/cfdemParticle/etc/functions.sh
+
 #--------------------------------------------------------------------------------#
 #- define variables
 casePath="$(dirname "$(readlink -f ${BASH_SOURCE[0]})")"
@@ -20,6 +24,7 @@ machineFileName="none"   # yourMachinefileName | none
 debugMode="off"          # on | off| strict
 reconstuctCase="true"    # true | false
 testHarnessPath="$CFDEM_TEST_HARNESS_PATH"
+
 #--------------------------------------------------------------------------------#
 #- call function to run a parallel CFD-DEM case
 parCFDDEMrun $logpath $logfileName $casePath $headerText $solverName $nrProcs $machineFileName $debugMode $reconstuctCase
